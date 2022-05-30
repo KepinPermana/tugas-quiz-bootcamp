e69375d524f3b5299853060485e348d797ff62f5
unknown <kepinpermana98@gmail.com>
2022-05-30 22:09:32 +0700

    first commit

---
+++ quiz-3/01-MaxNumber.js
@@ -0,0 +1,15 @@
+const numbers = [44,131,335,223,21,66,87];
+// This syntax is not allowed. The result is NaN.
+
+const max = Math.max(...numbers);
+const index = numbers.indexOf(max);
+console.log(max);
+
+
+function maxNumber(arrays){
+
+  return max;  
+
+}
+
+console.log(maxNumber(numbers));
